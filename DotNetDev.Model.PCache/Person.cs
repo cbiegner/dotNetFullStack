84d449faec686d32c33986ab4abdf39d9746cc91
Christoph Biegner <cbiegner@web.de>
2018-08-14 08:11:09 +0200

    Service added

--- DotNetDev.Model.PCache/Person.cs
+++ DotNetDev.Model.PCache/Person.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNetDev.Model.PCache
+﻿namespace DotNetDev.Model
 {
-    public class Person
-    {
-    }
+	public class Person
+	{
+		public long Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public Address Address { get; set; }
+		public Communication Communication { get; set; }
+
+		public Person()
+		{
+			Name = string.Empty;
+			Surname = string.Empty;
+		}
+	}
+
+	public enum AddressType
+	{
+		Private = 1, Business = 2, Other = 0
+	}
+
+	public class Address
+	{
+		public AddressType Type { get; set; }
+		public string Street { get; set; }
+		public string City { get; set; }
+		public string ZIP { get; set; }
+	}
+
+	public enum CommunicationType
+	{
+		Phone = 1, Email = 2, Other = 0
+	}
+
+	public class Communication
+	{
+		public CommunicationType Type { get; set; }
+		public string Value { get; set; }
+	}
 }
---
+++ DotNetDev.Service.PCache/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetDev.Service.PCache.WebApiApplication" Language="C#" %>
